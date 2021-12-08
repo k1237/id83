84aa27da6fbbde7b4f8eddc3ba49aff36cc4c2e7
k1237 <biz-kouta1237@outlook.jp>
2021-12-08 10:34:32 +0900

    compositionAPI化

--- resources/js/components/ExcutedComponent.vue
+++ resources/js/components/ExcutedComponent.vue
@@ -1,62 +1,69 @@
 <template>
   <form method="post" action="./delete">
-    <input type="hidden" name="_token" :value="csrf" />
-    <input type="hidden" name="id" v-model="id" />
-    <input type="hidden" name="user_id" v-model="users.id" />
+    <input type="hidden" name="_token" :value="data.csrf" />
+    <input type="hidden" name="id" v-model="data.id" />
+    <input type="hidden" name="user_id" v-model="data.users.id" />
     <div class="mb-3 d-flex">
       <input
         type="text"
         class="form-control col-xs-4"
         name="idea_text"
         style="width: 80%"
-        v-model="value"
+        v-model="data.value"
         readonly
       />
-      <button type="submit" name="delete" class="btn btn-danger ml-2">
-        ×
-      </button>
+      <button type="submit" name="delete" class="btn btn-danger ml-2">×</button>
     </div>
   </form>
 </template>
 
 <script>
 import axios from "axios";
-export default {
-  data() {
-    return {
-      value: "",
-      users: [],
-      id: "",
-      csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-    };
-  },
+import { defineComponent, reactive, onMounted } from "vue";
 
-  mounted() {
-    this.settingData();
-    this.value = this.Idea.idea;
-    this.id = this.Idea.id;
-    if (!this.csrf) {
-      console.warn(
-        'The CSRF token is missing. Ensure that the HTML header includes the following: <meta name="csrf-token" content="{{ csrf_token() }}">'
-      );
-    }
-  },
-
+export default defineComponent({
   props: {
     Number: Number,
     Idea: Object,
   },
 
-  methods: {
-    User: async function () {
+  setup: (props) => {
+    //data
+    const data = reactive({
+      value: "",
+      users: [],
+      id: "",
+      csrf: document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content"),
+    });
+
+    //mounted
+    onMounted(() => {
+      settingData();
+      data.value = props.Idea.idea;
+      data.id = props.Idea.id;
+      if (!data.csrf) {
+        console.warn(
+          'The CSRF token is missing. Ensure that the HTML header includes the following: <meta name="csrf-token" content="{{ csrf_token() }}">'
+        );
+      }
+    });
+
+    //methods
+    const User = async function () {
       await axios.get("http://127.0.0.1:8000/api/profile").then((response) => {
-        this.users = response.data;
+        data.users = response.data;
       });
-    },
-    settingData: async function () {
-      await this.User();
-    },
+    };
+
+    const settingData = async function () {
+      await User();
+    };
+
+    return { data, onMounted, User, settingData };
   },
-};
+});
 </script>
+
 
