243e02f1fb6d3536754870ded7a0fe8327c13099
k1237 <biz-kouta1237@outlook.jp>
2021-12-15 10:01:21 +0900

    null

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -46,16 +46,9 @@ class MemoController extends Controller
 
     public function memo()
     {
-        $user = auth()->user();
-        $memo = Memo::where('user_id', $user['id'])->first()->memo;
-        
-        if($memo!=null){
-            return $memo;
-        }else if($memo=null){
-            
-        }
+        $user = auth()->user()??null;
+        return Memo::where('user_id', $user['id'])->first()->memo ?? null;
     }
-       
     
  
 }
