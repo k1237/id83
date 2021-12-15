2caaaf77e092e3eadff372894d41f52af30d0a3c
k1237 <biz-kouta1237@outlook.jp>
2021-12-15 15:17:04 +0900

    aa

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -56,8 +56,11 @@ class MemoController extends Controller
        
 
         //500エラーが消えるが表示はされないAPIは表示される
-        $user = auth()->user()->id ?? null;
-        $memo = Memo::where('user_id', $user)->first() ?? null;
-        return $memo->memo??null ;
+        // $user = auth()->user()->id ?? null;
+        // $memo = Memo::where('user_id', $user)->first() ?? null;
+        // return $memo->memo??null ;
+
+        $token = $request->user()->createToken($request->token_name);
+        return ['token' => $token->plainTextToken];
     }
 }
