b63f0d950d872da070dbe24eb49486b759a27357
k1237 <biz-kouta1237@outlook.jp>
2021-12-15 14:17:53 +0900

    aa

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -50,14 +50,14 @@ class MemoController extends Controller
         // return Memo::first()->memo;
 
         //これも反映される
-        // $memo =Memo::where('user_id',1)->first()->memo ?? null;
-        // return $memo;
+        $memo =Memo::where('user_id',1)->first()->memo ?? null;
+        return $memo;
 
        
 
         //500エラーが消えるが表示はされないAPIは表示される
-        $user = auth()->user()->id ?? null;
-        $memo = Memo::where('user_id', $user)->first() ?? null;
-        return $memo->memo??null ;
+        // $user = auth()->user()->id ?? null;
+        // $memo = Memo::where('user_id', $user)->first() ?? null;
+        // return $memo->memo??null ;
     }
 }
