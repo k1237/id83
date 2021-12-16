1797ca63f17b0de7b33685886fe7b8b91ca6e52d
k1237 <biz-kouta1237@outlook.jp>
2021-12-16 09:04:18 +0900

    aa

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -52,9 +52,7 @@ class MemoController extends Controller
         //これも反映される
         // $memo =Memo::where('user_id',1)->first()->memo ?? null;
         // return $memo;
-
        
-
         //500エラーが消えるが表示はされないAPIは表示される
         $user = auth()->user()->id ?? null;
         $memo = Memo::where('user_id', $user)->first() ?? null;
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth')->get('/memo',[MemoController::class, 'memo']);
+Route::get('/memo',[MemoController::class, 'memo']);
 
 
